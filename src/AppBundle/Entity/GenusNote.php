be078893deb67a37d2446f3a89bbc12e2a16ba17
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-24 16:47:33 +0200

    Add genus_note entity

---
+++ src/AppBundle/Entity/GenusNote.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * User: bostaf
+ * Date: 24.06.16
+ * Time: 16:28
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus_note")
+ */
+class GenusNote
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $usernameAvatarFilename;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsernameAvatarFilename()
+    {
+        return $this->usernameAvatarFilename;
+    }
+
+    /**
+     * @param mixed $usernameAvatarFilename
+     */
+    public function setUsernameAvatarFilename($usernameAvatarFilename)
+    {
+        $this->usernameAvatarFilename = $usernameAvatarFilename;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param mixed $note
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
